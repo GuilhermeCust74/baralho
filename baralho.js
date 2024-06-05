d228672310ac2d1dec9250afff8a05e89aaf7d87
Botas <143020046+GuilhermeCust74@users.noreply.github.com>
2024-06-05 12:28:57 -0300

    Add files via upload

---
+++ baralho.js
@@ -0,0 +1,19 @@
+const Baralho = {
+    naipes: ['Paus', 'Ouros', 'Copas', 'Espadas'],
+    valores: ['Ás', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Valete', 'Dama', 'Rei'],
+}
+
+const RegrasTruco = {
+    numJogadores: 4,
+    numCartasPorJogador: 3,
+    pontosVitoria: 12,
+    jogadoresPorTime: 2,
+    limiteRodadas: 3,
+    rodadaBonus: true,
+    empateDesempate: true,
+    numCartasMao: 3,
+    numCartasValete: 4,
+    numCartasDez: 3,
+    numCartasSete: 2,
+    numCartasAs: 1
+  };
